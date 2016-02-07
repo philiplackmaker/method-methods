f4ffb32c721132ff3c2dc3b486d5537be6133092
Philip Lackmaker <philip@ustwo.co.uk>
2016-02-08 09:54:23 +1100

    updare

---
+++ _site/js/master.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+    dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+  });
+});
